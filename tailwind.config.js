21f67a17af9a5031ae3821c0f7c4801b76b4e315
Maria Pedersen <math0008@stud.kea.dk>
2025-12-18 02:38:31 +0100

    tailwind confiq

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx,html}"],
+  safelist: ["w-[100px]", "h-[100px]", "w-500", "w-244", "h-218"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
